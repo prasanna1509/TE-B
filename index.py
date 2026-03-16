cf61e45757948ddd94b19a7945832c38cd90571a
prasanna1509 <prasuwarang15@gmail.com>
2026-03-16 13:22:21 +0530

    Update index.py

--- index.py
+++ index.py
@@ -11,3 +11,11 @@ for i in range(n, 0, -1):
     for j in range(i):
         print("*", end="")
     print()
+
+print("  *****  ")
+print(" *     * ")
+print("*  * *  *")
+print("*       *")
+print("*  ---  *")
+print(" *     * ")
+print("  *****  ")
