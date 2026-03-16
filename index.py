c23c2f0cdd958264e0d370e5511b24622277713d
prasanna1509 <prasuwarang15@gmail.com>
2026-03-16 13:04:42 +0530

    Update index.py

--- index.py
+++ index.py
@@ -4,3 +4,11 @@ for i in range(n):
     for j in range(n):
         print("*", end="")
     print()
+
+
+m = 5
+
+for a in range(1, m+1):
+    for b in range(a):
+        print("*", end="")
+    print()
